07d51c216c3ff5f55090e15710814ba128ff250b
pppppptttttt <pasha.kort@gmail.com>
2025-03-15 17:58:20 +0300

    Forgot to remove Client/ directory

--- Client/app/src/main/java/ru/hse/online/client/common/Constants.kt
+++
@@ -1,3 +0,0 @@
-package ru.hse.online.client.common
-
-const val ui_tag: String = "online client ui"
--- Client/app/src/main/java/ru/hse/online/client/interactor/AuthInteractor.kt
+++
@@ -1,8 +0,0 @@
-package ru.hse.online.client.interactor
-
-class AuthInteractor {
-
-    fun handleLogIn() { TODO() }
-
-    fun handleSignUp() { TODO() }
-}
