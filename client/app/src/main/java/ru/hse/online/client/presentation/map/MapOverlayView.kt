d29c582e779f7dd2fc4ff7a051aa9af06f915312
Qwertst <aydreq@mail.ru>
2025-06-14 18:44:18 +0300

    fix save path dialog

--- client/app/src/main/java/ru/hse/online/client/presentation/map/MapOverlayView.kt
+++ client/app/src/main/java/ru/hse/online/client/presentation/map/MapOverlayView.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -359,14 +360,14 @@ fun PathSavingDialog(
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
             modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .height(200.dp)
+                .fillMaxWidth(0.6f)
+                .wrapContentSize()
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
             Column(
                 modifier = Modifier
-                    .fillMaxSize(),
+                    .wrapContentSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
